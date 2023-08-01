73b98524277a7a27511fcc34f613ab543d55c804
bharat bardiya <bharatbardiya123@gmail.com>
2023-08-01 19:31:02 +0530

    deployment 0

--- backend/app.js
+++ backend/app.js
@@ -4,20 +4,21 @@ const cookieParser = require("cookie-parser");
 const bodyparser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./middlewares/errors");
+const path = require("path");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
 
-app.use(function (req, res, next) {
-    // console.log(process.env.FRONTEND_BASE_URL);
-    res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_BASE_URL);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
+// app.use(function (req, res, next) {
+//     // console.log(process.env.FRONTEND_BASE_URL);
+//     res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_BASE_URL);
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     next();
+// });
 
 //Import all routes
 const products = require("./routes/product");
@@ -27,6 +28,13 @@ const order = require("./routes/order");
 app.use("/api/v1", products);
 app.use("/api/v1", auth);
 app.use("/api/v1", order);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
 
 // Middleware to handle errors
 app.use(errorMiddleware);
